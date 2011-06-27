29ff79efb089e4c11fb5fd7be13872e16635f973
Alexey Veretennikov <alexey@veroveli.com>
2011-06-27 03:25:24 +0200

    Implemented support of syntax rules while reducing grammar rules. Now the parser prepares the list of CONSes as a result

---
+++ main.c
@@ -0,0 +1,68 @@
+/* -*- Mode: C; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "sexpgrammar.h"
+#include "sexplexer.h"
+#include "sexpparser.h"
+
+const int block_size = 10;
+
+int main(int argc, const char* argv[])
+{
+  /* input file */
+  FILE* file = stdin;
+
+  /* buffer to read to */
+  char* read_buffer = calloc(block_size+1,1);
+  char* p;
+  
+  /* auxulary counters */
+  int read_chunk = 0,read = 0;
+  int read_from_file = argc > 1;
+
+  /* list of tokens */
+  sexp_token_cont_item *head = 0;
+  sexp_token* token = 0;
+
+  /* parser result */
+  sexp_item* sexp;
+  
+  /* set the file to read from */
+  if ( read_from_file )
+    file = fopen(argv[1],"rt");
+
+  /* read file contents  */
+  while(!feof(stdin))
+  {
+    read_chunk = fread(read_buffer+read,1,block_size, file);
+    read += read_chunk;
+    
+    read_buffer = (char*)realloc(read_buffer,read + block_size);
+  }
+  read_buffer[read] = '\0';
+  
+  /* close the file if necessary */
+  if ( read_from_file)
+    fclose(file);
+
+  /* 1. Lexer */
+  p = read_buffer;
+  token = read_sexp_token(&p);  
+  if (token)
+    head = sexp_token_cont_item_alloc(token);
+  while ( (token = read_sexp_token(&p)))
+    sexp_token_list_add(head,token);
+
+  /* 2. Parser */
+  sexp = sexp_parser(head);
+  /* 3. Print output */
+  sexp_item_print(sexp);
+  
+  /* free allocated memory */
+  free(read_buffer);    
+  head = sexp_token_cont_list_free(head);
+  
+  
+  return 0;
+}
